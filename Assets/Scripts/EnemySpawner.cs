612d9f62781d1eaed89b5c40420dcbbb7c6a6bd1
Phoques <dylan.munro6@studytafensw.edu.au>
2022-11-11 12:22:40 +1100

    Spawner added
    Enemy spawns randomly, however the enemies sometimes within the camera viewport, this needs to be fixed.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject _mob;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+    public GameObject Camera;
+
+
+
+    void Update()
+    {
+        SpawnEnemies();
+    }
+
+
+    
+
+    void SpawnEnemies()
+    {
+        
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-36f, 62);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(_mob, whereToSpawn, Quaternion.identity);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "SpawnRestriction")
+        {
+            Destroy(_mob);
+        }
+    }
+}
